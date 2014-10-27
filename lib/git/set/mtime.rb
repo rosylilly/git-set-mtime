ad02b4718399daa197ed0af4dea025feef3ceacf
Sho Kusano <sho-kusano@mgnt-inc.jp>
2014-10-27 22:49:12 +0900

    Show files

--- lib/git/set/mtime.rb
+++ lib/git/set/mtime.rb
@@ -1,9 +1,21 @@
+require 'thor'
+require 'rugged'
 require "git/set/mtime/version"
 
-module Git
-  module Set
-    module Mtime
-      # Your code goes here...
+module Git::Set::Mtime
+  class CLI < Thor
+    desc 'apply', 'apply mtime to files'
+    def apply
+      repo = Rugged::Repository.new(Dir.pwd)
+      head = repo.head
+      target = head
+      while target.respond_to?(:target)
+        target = target.target
+      end
+
+      target.tree.each { |e| puts e }
     end
+
+    default_task :apply
   end
 end
