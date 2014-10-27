4a278fec495cc1675e1e1c73cb7f92d0f926be88
Sho Kusano <sho-kusano@mgnt-inc.jp>
2014-10-27 21:30:53 +0900

    Init repo

---
+++ lib/git/set/mtime.rb
@@ -0,0 +1,9 @@
+require "git/set/mtime/version"
+
+module Git
+  module Set
+    module Mtime
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/git/set/mtime/version.rb
@@ -0,0 +1,7 @@
+module Git
+  module Set
+    module Mtime
+      VERSION = "0.0.1"
+    end
+  end
+end
