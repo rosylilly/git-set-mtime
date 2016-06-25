db24c4c3f5f8d786f87d6071bfd4e2af857c63b6
Songmu <y.songmu@gmail.com>
2016-06-25 13:24:18 +0900

    concurrency

--- main.go
+++ main.go
@@ -5,11 +5,41 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 	"time"
 )
 
 const rfc2822 = "Mon, 2 Jan 2006 15:04:05 -0700"
+
+type mtimes struct {
+	store map[string]time.Time
+	mu    *sync.Mutex
+}
+
+func newMtimes() *mtimes {
+	return &mtimes{
+		store: make(map[string]time.Time),
+		mu:    &sync.Mutex{},
+	}
+}
+
+func (m *mtimes) setIfAfter(dir string, mTime time.Time) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if other, ok := m.store[dir]; ok {
+		if mTime.After(other) {
+			// file mTime is more recent than previous seen for 'dir'
+			m.store[dir] = mTime
+		}
+	} else {
+		// first occurrence of dir
+		m.store[dir] = mTime
+	}
+	m.store[dir] = mTime
+}
 
 func main() {
 	lsFiles := exec.Command("git", "ls-files", "-z")
@@ -20,63 +50,71 @@ func main() {
 		os.Exit(1)
 	}
 
-	dirMTimes := map[string]time.Time{}
-
+	dirMTimes := newMtimes()
+	paralevel := runtime.GOMAXPROCS(-1) * 2
 	files := strings.Split(strings.TrimRight(string(out), "\x00"), "\x00")
-	for _, file := range files {
-		gitLog := exec.Command("git", "log", "-1", "--date=rfc2822", "--format=%cd", file)
+	ch := make(chan string, paralevel)
+	var wg sync.WaitGroup
+	for i := 1; i < paralevel; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for file := range ch {
+				gitLog := exec.Command("git", "log", "-1", "--date=rfc2822", "--format=%cd", file)
+				out, err := gitLog.Output()
 
-		out, err := gitLog.Output()
-
-		if err != nil {
-			fmt.Fprint(os.Stderr, err)
-			os.Exit(1)
-		}
-
-		mStr := strings.TrimSpace(strings.TrimLeft(string(out), "Date:"))
-		mTime, err := time.Parse(rfc2822, mStr)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s on %s", err, file)
-			os.Exit(1)
-		}
-
-		// Loop over each directory in the path to `file`, updating `dirMTimes`
-		// to take the most recent time seen.
-		dir := filepath.Dir(file)
-		for {
-			if other, ok := dirMTimes[dir]; ok {
-				if mTime.After(other) {
-					// file mTime is more recent than previous seen for 'dir'
-					dirMTimes[dir] = mTime
+				if err != nil {
+					fmt.Fprint(os.Stderr, err)
+					os.Exit(1)
 				}
-			} else {
-				// first occurrence of dir
-				dirMTimes[dir] = mTime
+
+				mStr := strings.TrimSpace(strings.TrimLeft(string(out), "Date:"))
+				mTime, err := time.Parse(rfc2822, mStr)
+
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s on %s", err, file)
+					os.Exit(1)
+				}
+
+				// Loop over each directory in the path to `file`, updating `dirMTimes`
+				// to take the most recent time seen.
+				dir := filepath.Dir(file)
+				for {
+					dirMTimes.setIfAfter(dir, mTime)
+
+					// Remove one directory from the path until it isn't changed anymore ("." == ".")
+					if dir == filepath.Dir(dir) {
+						break
+					}
+					dir = filepath.Dir(dir)
+				}
+
+				err = lutimes(file, mTime, mTime)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s on %s", err, file)
+					os.Exit(1)
+				}
+				fmt.Printf("%s: %s\n", file, mTime)
 			}
+		}()
+	}
+	for _, file := range files {
+		ch <- file
+	}
+	close(ch)
+	wg.Wait()
 
-			// Remove one directory from the path until it isn't changed anymore
-			if dir == filepath.Dir(dir) {
-				break
+	for dir, mTime := range dirMTimes.store {
+		wg.Add(1)
+		go func(dir string, mTime time.Time) {
+			defer wg.Done()
+			err = lutimes(dir, mTime, mTime)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s on %s", err, dir)
+				os.Exit(1)
 			}
-			dir = filepath.Dir(dir)
-		}
-
-		err = lutimes(file, mTime, mTime)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s on %s", err, file)
-			os.Exit(1)
-		}
-
-		fmt.Printf("%s: %s\n", file, mTime)
+			fmt.Printf("%s: %s\n", dir, mTime)
+		}(dir, mTime)
 	}
-
-	for dir, mTime := range dirMTimes {
-		err = lutimes(dir, mTime, mTime)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s on %s", err, dir)
-			os.Exit(1)
-		}
-		fmt.Printf("%s: %s\n", dir, mTime)
-	}
+	wg.Wait()
 }
